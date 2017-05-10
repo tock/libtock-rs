604e26f7566bc3bbe2670a0539e608ad525ad474
Amit Aryeh Levy <amit@amitlevy.com>
2017-05-10 14:42:00 -0400

    Asynchronous blink example

---
+++ examples/blink_async.rs
@@ -0,0 +1,14 @@
+#![no_std]
+
+extern crate tock;
+
+use tock::{led, timer};
+
+extern fn timer_event(_: usize, _: usize, _: usize, _: usize) {
+    led::toggle(0);
+}
+
+fn main() {
+    timer::subscribe(timer_event, 0);
+    timer::start_repeating(500);
+}
