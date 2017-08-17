0652cd10aca75c0821a6efa09c9d20c3fe5726bc
Amit Aryeh Levy <amit@amitlevy.com>
2017-08-17 14:42:11 -0700

    Refactor BLE-ESS service into wrapper module

--- examples/ble_ess.rs
+++ examples/ble_ess.rs
@@ -4,41 +4,20 @@
 extern crate alloc;
 extern crate tock;
 
-use alloc::boxed::Box;
-use alloc::String;
 use alloc::fmt::Write;
 use tock::console::Console;
-use tock::ipc;
+use tock::ipc::ble_ess::{self, ReadingType};
 use tock::sensors::*;
-
-fn setup_ipc() -> Result<(ipc::Client, Box<[u8]>), ()> {
-    let mut ipc_client = ipc::Client::new(
-                String::from("org.tockos.services.ble-ess"))?;
-    let shared_vec = ipc_client.share(5)?;
-    Ok((ipc_client, shared_vec))
-}
-
-fn set_ipc_slice<I: Into<i32>>(slice: &mut [u8], sensor_type: u32, sensor_data: I) {
-    let sensor_data = Into::<i32>::into(sensor_data) as u32;
-    slice[0..4].copy_from_slice(&[(sensor_type & 0xff) as u8,
-                                    ((sensor_type >> 8) & 0xff) as u8,
-                                    ((sensor_type >> 16) & 0xff) as u8,
-                                    ((sensor_type >> 24) & 0xff) as u8]);
-    slice[4..8].copy_from_slice(&[(sensor_data & 0xff) as u8,
-                                ((sensor_data >> 8) & 0xff) as u8,
-                                ((sensor_data >> 16) & 0xff) as u8,
-                                ((sensor_data >> 24) & 0xff) as u8]);
-}
 
 fn main() {
     let mut console = Console::new();
     write!(&mut console, "Starting BLE ESS\n").unwrap();
 
-    let (mut ipc_client, mut updates) = match setup_ipc() {
-        Ok((c, s)) => (c, s),
+    let mut ess = match ble_ess::connect() {
+        Ok(ess) => ess,
         _ => {
             write!(&mut console, "BLE IPC Service not installed\n").unwrap();
-            return;
+            return
         }
     };
     write!(&mut console, "Found BLE IPC Service\n").unwrap();
@@ -51,25 +30,22 @@ fn main() {
         // Temperature
         let temp = temperature.read();
         write!(&mut console, "Temperature: {}\n", temp).unwrap();
-        set_ipc_slice(&mut *updates, 0, temp);
-        if let Err(_) = ipc_client.notify() {
-            write!(&mut console, "Failed to send temperature\n").unwrap_or(());
+        if let Err(_) = ess.set_reading(ReadingType::Temperature, temp) {
+            write!(&mut console, "Failed to set temperature\n").unwrap_or(());
         }
         
         // Light
         let lx = light.read();
         write!(&mut console, "Light:       {}\n", lx).unwrap();
-        set_ipc_slice(&mut *updates, 1, lx);
-        if let Err(_) = ipc_client.notify() {
-            write!(&mut console, "Failed to send light\n").unwrap_or(());
+        if let Err(_) = ess.set_reading(ReadingType::Light, lx) {
+            write!(&mut console, "Failed to set temperature\n").unwrap_or(());
         }
 
         // Humidity
         let humid = humidity.read();
         write!(&mut console, "Humidity:    {}\n", humid).unwrap();
-        set_ipc_slice(&mut *updates, 2, humid);
-        if let Err(_) = ipc_client.notify() {
-            write!(&mut console, "Failed to send humidity\n").unwrap_or(());
+        if let Err(_) = ess.set_reading(ReadingType::Humidity, humid) {
+            write!(&mut console, "Failed to set temperature\n").unwrap_or(());
         }
 
         tock::timer::delay_ms(5000);
--- src/ipc/mod.rs
+++ src/ipc/mod.rs
@@ -1,3 +1,4 @@
 pub mod client;
+pub mod ble_ess;
 
 pub use self::client::*;
