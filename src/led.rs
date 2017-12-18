8bde7ea702d4be39f46ed483ed68fab3ce1ece68
Philipp Vollmer <briefe@kebes.de>
2017-12-18 10:16:01 +0100

    Fix led driver number

--- src/led.rs
+++ src/led.rs
@@ -4,27 +4,28 @@ const COUNT: u32 = 0;
 const ON: u32 = 1;
 const OFF: u32 = 2;
 const TOGGLE: u32 = 3;
+const LED_DRIVER: u32 = 2;
 
 pub fn count() -> isize {
     unsafe {
-        command(8, COUNT, 0)
+        command(LED_DRIVER, COUNT, 0)
     }
 }
 
 pub fn on(led_num: u32) {
     unsafe {
-        command(8, ON, led_num as isize);
+        command(LED_DRIVER, ON, led_num as isize);
     }
 }
 
 pub fn off(led_num: u32) {
     unsafe {
-        command(8, OFF, led_num as isize);
+        command(LED_DRIVER, OFF, led_num as isize);
     }
 }
 
 pub fn toggle(led_num: u32) {
     unsafe {
-        command(8, TOGGLE, led_num as isize);
+        command(LED_DRIVER, TOGGLE, led_num as isize);
     }
 }
