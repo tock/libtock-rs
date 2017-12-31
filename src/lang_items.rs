3597431c3c1bef45b8d4a1871c7b26f7d00c2c66
Woyten <woyten.tielesch@online.de>
2017-12-31 21:42:53 +0100

    Flash LEDs when panicking

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -16,21 +16,54 @@
 // The final piece is that the entry point of our program, _start, has to call
 // `rustc_main`. That's covered by the `_start` function in the root of this
 // crate.
+use led;
+use timer;
 #[lang = "start"]
-extern "C" fn start(
-    main: fn(),
-    _argc: isize,
-    _argv: *const *const u8,
-) -> isize {
+extern "C" fn start(main: fn(), _argc: isize, _argv: *const *const u8) -> isize {
     main();
 
     0
 }
 
 #[lang = "eh_personality"]
-extern "C" fn eh_personality() {}
+extern "C" fn eh_personality() {
+    circle_leds(); // TODO: Make NRF52dK specific
+}
+
 
 #[lang = "panic_fmt"]
 unsafe extern "C" fn rust_begin_unwind() {
-    loop {}
+    flash_all_leds(); // TODO: Make NRF52DK specific
+}
+
+fn circle_leds() {
+    loop {
+        led::on(0);
+        timer::delay_ms(100);
+        led::off(0);
+        led::on(1);
+        timer::delay_ms(100);
+        led::off(1);
+        led::on(3);
+        timer::delay_ms(100);
+        led::off(3);
+        led::on(2);
+        timer::delay_ms(100);
+        led::off(2);
+    }
+}
+
+fn flash_all_leds() {
+    loop {
+        led::on(0);
+        led::on(1);
+        led::on(2);
+        led::on(3);
+        timer::delay_ms(100);
+        led::off(0);
+        led::off(1);
+        led::off(2);
+        led::off(3);
+        timer::delay_ms(100);
+    }
 }
