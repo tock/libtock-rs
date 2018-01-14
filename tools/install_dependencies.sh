94d21edfca63e24cd68d854fe17e885c88441785
Woyten <woyten.tielesch@online.de>
2018-01-14 17:16:58 +0100

    Force specific xargo and elf2tbf version

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -2,5 +2,5 @@
 
 set -eux
 
-cargo install -f xargo
-cargo install -f --git https://github.com/helena-project/tock.git elf2tbf
+cargo install -f --git https://github.com/japaric/xargo --vers 0.3.10
+cargo install -f --git https://github.com/helena-project/tock.git elf2tbf --rev 09ed0a57c58d84595910b410494240ed995577ab
