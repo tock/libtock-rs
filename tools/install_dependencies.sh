e05178d123f0a5845eaf0f639d76a4b805ecb167
Woyten <woyten.tielesch@online.de>
2018-02-02 21:04:36 +0100

    Speed up build by including tock as a submodule

--- tools/install_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-cargo install -f --git https://github.com/japaric/xargo --vers 0.3.10
-cargo install -f --git https://github.com/helena-project/tock.git elf2tbf --rev 09ed0a57c58d84595910b410494240ed995577ab
---
+++ tools/install_xargo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -eux
+
+cargo install -f --git https://github.com/japaric/xargo --vers 0.3.10
