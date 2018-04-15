5c30b19002a0b261ada3765055d0e7a3ba2b892c
Woyten <woyten.tielesch@online.de>
2018-04-15 12:20:31 +0200

    Match future toolchain of Tock OS

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -1,21 +1,23 @@
-/// Lang item required to make the normal `main` work in applications
-// This is how the `start` lang item works:
-// When `rustc` compiles a binary crate, it creates a `main` function that looks
-// like this:
-//
-// ```
-// #[export_name = "main"]
-// pub extern "C" fn rustc_main(argc: isize, argv: *const *const u8) -> isize {
-//     start(main, argc, argv)
-// }
-// ```
-//
-// Where `start` is this function and `main` is the binary crate's `main`
-// function.
-//
-// The final piece is that the entry point of our program, _start, has to call
-// `rustc_main`. That's covered by the `_start` function in the root of this
-// crate.
+//! Lang item required to make the normal `main` work in applications
+//!
+//! This is how the `start` lang item works:
+//! When `rustc` compiles a binary crate, it creates a `main` function that looks
+//! like this:
+//!
+//! ```
+//! #[export_name = "main"]
+//! pub extern "C" fn rustc_main(argc: isize, argv: *const *const u8) -> isize {
+//!     start(main, argc, argv)
+//! }
+//! ```
+//!
+//! Where `start` is this function and `main` is the binary crate's `main`
+//! function.
+//!
+//! The final piece is that the entry point of our program, _start, has to call
+//! `rustc_main`. That's covered by the `_start` function in the root of this
+//! crate.
+
 use led;
 use timer;
 use timer::Duration;
