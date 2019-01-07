57247655e1721927d9be206237414308935a0a3b
Johnathan Van Why <jrvanwhy@google.com>
2019-01-07 15:45:52 -0800

    Refactor/clean up the entry point, to make future modifications easier.
    The entry point, which was previously written mostly in Rust, is now split into two procedures. The first procedure is a pure-assembly procedure that sets up the stack pointer, and the second is a Rust function that performs higher-level setup. This fixes an issue with the existing setup: the existing _start function uses local variables (which may be placed on the stack) before it initializes the stack pointer.
    
    The next step is to copy the .data section from flash into RAM and perform relocations on it. This may require modifying elf2tab to copy over relocation information.

--- src/entry_point.rs
+++ src/entry_point.rs
@@ -1,3 +1,17 @@
+// Copyright 2018 The Tock OS Developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 extern crate linked_list_allocator;
 
 use self::linked_list_allocator::Heap;
@@ -5,6 +19,7 @@ use crate::syscalls;
 use core::alloc::Alloc;
 use core::alloc::GlobalAlloc;
 use core::alloc::Layout;
+use core::intrinsics;
 use core::mem;
 use core::ptr;
 use core::ptr::NonNull;
@@ -47,7 +62,30 @@ unsafe impl GlobalAlloc for TockAllocator {
     }
 }
 
-/// Tock programs' entry point
+// _start and rust_start are the first two procedures executed when a Tock
+// application starts. _start is invoked directly by the Tock kernel; it
+// performs stack setup then calls rust_start. rust_start performs data
+// relocation and sets up the heap before calling the rustc-generated main.
+// rust_start and _start are tightly coupled: the order of rust_start's
+// parameters is designed to simplify _start's implementation.
+//
+// The memory layout set up by these methods is as follows:
+//
+//     +----------------+ <- app_heap_break
+//     | Heap           |
+//     +----------------| <- heap_bottom
+//     | Data (globals) |
+//     +----------------+ <- stack_top
+//     | Stack          |
+//     | (grows down)   |
+//     +----------------+ <- mem_start
+//
+// app_heap_break and mem_start are given to us by the kernel. The stack size is
+// determined using pointer text_start, and is used with mem_start to compute
+// stack_top.
+
+/// Tock programs' entry point. Called by the kernel at program start. Sets up
+/// the stack then calls rust_start() for the remainder of setup.
 #[doc(hidden)]
 #[no_mangle]
 #[naked]
@@ -56,24 +94,47 @@ pub unsafe extern "C" fn _start(
     text_start: usize,
     mem_start: usize,
     _memory_len: usize,
-    _app_heap_break: usize,
+    app_heap_break: usize,
+) -> ! {
+    asm!("
+        // Initialize the stack pointer. The stack pointer is computed as
+        // stack_size + mem_start plus padding to start the stack on a multiple
+        // of 8 bytes.
+        ldr ip, [r0, #36]  // ip = text_start->stack_size
+        add ip, ip, r1     // ip = text_start->stack_size + mem_start
+        add ip, #7         // ip = text_start->stack_size + mem_start + 7
+        bic r1, ip, #7     // r1 = (text_start->stack_size + mem_start + 7) & ~0x7
+
+        // Call rust_start, passing text_start, stack_top, and app_heap_break to it.
+        mov sp, r1
+        bl rust_start"
+        :                                                              // No output operands
+        : "{r0}"(text_start) "{r1}"(mem_start) "{r3}"(app_heap_break)  // Input operands
+        : "cc" "ip" "lr" "memory" "r0" "r1" "r2" "r3" "r9" "sp"        // Clobbers
+        :                                                              // Options
+    );
+    intrinsics::unreachable();
+}
+
+/// Rust setup, called by _start. Uses the extern "C" calling convention so that
+/// the assembly in _start knows how to call it (the Rust ABI is not defined).
+/// Sets up the data segment (including relocations) and the heap, then calls
+/// into the rustc-generated main(). This cannot use mutable global variables or
+/// global references to globals until it is done setting up the data segment.
+#[no_mangle]
+pub unsafe extern "C" fn rust_start(
+    _text_start: usize, stack_top: usize, _skipped: usize, _app_heap_break: usize
 ) -> ! {
     extern "C" {
         // This function is created internally by`rustc`. See `src/lang_items.rs` for more details.
         fn main(argc: isize, argv: *const *const u8) -> isize;
     }
 
-    let stack_size = *(text_start as *const usize).offset(9);
-    let real_stack_top = mem_start + stack_size;
-    // Set the effective stack top below the real stack stop and use the space in between for the heap
-    let effective_stack_top = real_stack_top - HEAP_SIZE;
-    TockAllocator.init(effective_stack_top, real_stack_top);
+    // TODO: Copy over .data and perform relocations, *then* initialize the heap.
+    TockAllocator.init(stack_top, stack_top + HEAP_SIZE);
 
-    asm!("mov sp, $0" : : "r"(effective_stack_top) : "memory" :  "volatile" );
-
-    syscalls::memop(0, effective_stack_top + HEAP_SIZE);
-    syscalls::memop(11, effective_stack_top + HEAP_SIZE);
-    syscalls::memop(10, effective_stack_top);
+    syscalls::memop(10, stack_top);
+    syscalls::memop(11, stack_top + HEAP_SIZE);
 
     main(0, ptr::null());
 
