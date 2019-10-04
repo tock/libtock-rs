5bd6a3c8e14733458b7548a1440982ffb05734ab
Guillaume Endignoux <guillaumee@google.com>
2019-10-04 16:30:39 +0200

    Fix typo.

--- examples/blink_random.rs
+++ examples/blink_random.rs
@@ -7,7 +7,7 @@ use libtock::timer::Duration;
 
 fn main() {
     let num_leds = led::count();
-    // blink_nimble assumes 4 leds.
+    // blink_nibble assumes 4 leds.
     assert_eq!(num_leds, 4);
 
     let mut buf = [0; 64];
@@ -15,16 +15,16 @@ fn main() {
         assert!(rng::fill_buffer(&mut buf));
 
         for &x in buf.iter() {
-            blink_nimble(x);
+            blink_nibble(x);
             timer::sleep(Duration::from_ms(100));
-            blink_nimble(x >> 4);
+            blink_nibble(x >> 4);
             timer::sleep(Duration::from_ms(100));
         }
     }
 }
 
 // Takes the 4 least-significant bits of x, and turn the 4 leds on/off accordingly.
-fn blink_nimble(x: u8) {
+fn blink_nibble(x: u8) {
     for i in 0..4 {
         let led = led::get(i).unwrap();
         if (x >> i) & 1 != 0 {
