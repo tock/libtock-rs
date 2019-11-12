806de82f6aa8270a582df852e6eb58b433aa4680
torfmaster <briefe@kebes.de>
2019-11-12 13:54:55 +0100

    Make temperature driver async.  - amend temperature example

--- examples/temperature.rs
+++ examples/temperature.rs
@@ -1,20 +1,20 @@
 #![no_std]
 
+use core::executor;
 use core::fmt::Write;
 use libtock::console::Console;
-use libtock::syscalls;
 use libtock::temperature;
+use libtock::timer;
 
 fn main() {
     let mut console = Console::new();
 
-    let mut with_callback = temperature::with_callback(|result: isize| {
-        writeln!(console, "Temperature: {}", result).unwrap();
-    });
-
-    let _temperature = with_callback.start_measurement();
-
     loop {
-        syscalls::yieldk();
+        let result = executor::block_on(async {
+            timer::sleep(timer::Duration::from_ms(1000)).await;
+            temperature::measure_temperature().await
+        });
+
+        writeln!(console, "Temperature: {}", result).unwrap();
     }
 }
