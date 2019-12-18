dda9499c1b9ce43976e1adb016d722d3c1646232
Guillaume Endignoux <guillaumee@google.com>
2019-12-18 16:53:12 +0100

    Add more debugging information.

--- examples/now.rs
+++ examples/now.rs
@@ -11,20 +11,26 @@ use libtock::timer::Duration;
 
 #[libtock::main]
 async fn main() -> TockResult<()> {
-    let mut console = Console::new();
     const DELAY_MS: isize = 50;
+
+    let mut console = Console::new();
+    let mut previous_ticks = None;
 
     for i in 0.. {
         let mut timer_with_callback = timer::with_callback(|_, _| {});
         let timer = timer_with_callback.init()?;
         let ticks = timer.get_current_clock()?.num_ticks();
+        let frequency = timer.clock_frequency().hz();
         writeln!(
             console,
-            "[{}] Waited {} ms. Now is {:#010x} ticks",
+            "[{}] Waited {} ms. Now is {:#010x} ticks ({} ticks since last time at {} Hz)",
             i,
             i * DELAY_MS,
-            ticks
+            ticks,
+            previous_ticks.map(|previous| ticks - previous),
+            frequency
         )?;
+        previous_ticks = Some(ticks);
         timer::sleep(Duration::from_ms(DELAY_MS)).await?;
     }
 
