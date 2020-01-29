860e0bc40fc5de679c16ee21afa67cf838ceb6cd
Woyten <woyten.tielesch@online.de>
2020-01-29 21:17:14 +0100

    Enable support for multiple boards and architectures

--- run_hardware_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tests only run on a nRF52-DK board
-
-set -eux
-
-yes 0|tockloader uninstall --jlink --arch cortex-m4 --board nrf52dk --jtag-device nrf52 || true
-
-./run_example.sh hardware_test --dont-clear-apps
