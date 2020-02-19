e0e9eab73d18c840c1eb3de10ed65523cfd7abff
Laura Abbott <laura@labbott.name>
2020-02-19 16:46:03 -0500

    Update flash.sh to better handle RISC-V targets
    The current flash.sh script doesn't handle RISC-V targets which means that
    even if you build with cargo, the actual tbf binaries don't get built.
    Update the flash.sh script to handle this case.
    
    As part of this work, also update the linker scripts for RISC-V targets.
    These need to be updated to work the protected region argument in elf2tab

--- flash.sh
+++ flash.sh
@@ -4,8 +4,36 @@ set -eux
 
 artifact="$(basename $1)"
 rust_target_folder="$(readlink -f $(dirname $1)/../..)"
+
+case "${PLATFORM}" in
+    "nrf52"|"nrf52840")
+        tockloader_flags="--jlink --arch cortex-m4 --board nrf52dk --jtag-device nrf52"
+        binary_name=cortex-m4.elf
+        tockload=y
+        ;;
+    "hail")
+        tockloader_flags=""
+        binary_name=cortex-m4.elf
+        tockload=y
+        ;;
+    "riscv32")
+        tockloader_flags=""
+        binary_name=rv32imac.elf
+        tockload=n
+        ;;
+    "opentitan")
+        tockloader_flags=""
+        binary_name=rv32imc.elf
+        tockload=n
+        ;;
+    *)
+        echo "Unknown platform \"${PLATFORM}\""
+        exit 1
+        ;;
+esac
+
 libtock_target_path="${rust_target_folder}/tab/${PLATFORM}/${artifact}"
-elf_file_name="${libtock_target_path}/cortex-m4.elf"
+elf_file_name="${libtock_target_path}/${binary_name}"
 tab_file_name="${libtock_target_path}.tab"
 
 mkdir -p "${libtock_target_path}"
@@ -13,18 +41,10 @@ cp "$1" "${elf_file_name}"
 
 elf2tab -n "${artifact}" -o "${tab_file_name}" "${elf_file_name}" --stack 2048 --app-heap 1024 --kernel-heap 1024 --protected-region-size=64
 
-case "${PLATFORM}" in
-    "nrf52"|"nrf52840")
-        tockloader_flags="--jlink --arch cortex-m4 --board nrf52dk --jtag-device nrf52"
-        ;;
-    "hail")
-        tockloader_flags=""
-        ;;
-    *)
-        echo "Tockloader flags unknown for platform \"${PLATFORM}\""
-        exit 1
-        ;;
-esac
+if [ $tockload == "n" ]; then
+	echo "Skipping flashing for platform \"${PLATFORM}\""
+	exit 0
+fi
 
 tockloader uninstall ${tockloader_flags} || true
 tockloader install ${tockloader_flags} "${tab_file_name}"
