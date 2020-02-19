654a6574e4db7d3dd61f2867ad9a1acafce36a3e
Woyten <woyten.tielesch@online.de>
2020-02-19 11:41:07 +0100

    Run clippy before tests

--- run_all_checks.sh
+++ run_all_checks.sh
@@ -5,6 +5,6 @@ set -eux
 export PLATFORM=nrf52 # The specific platform doesn't matter for tests
 
 cargo fmt --all -- --check
-cargo test --workspace
 cargo clippy --workspace --all-targets
+cargo test --workspace
 ./build_examples.sh
