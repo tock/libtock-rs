4ace40f509f8a5734a991ccbfb282c3fe3075d0a
Vochescu Alexandru <alexvochescu@gmail.com>
2020-06-08 14:41:56 +0300

    Readd modifications

--- tools/flash.sh
+++ tools/flash.sh
@@ -3,7 +3,7 @@
 set -eux
 
 artifact="$(basename $1)"
-rust_target_folder="$(readlink -f $(dirname $1)/../..)"
+rust_target_folder="$(greadlink -f $(dirname $1)/../..)"
 if [ -z $APP_HEAP_SIZE ]; then
 	echo "Set APP_HEAP_SIZE to a value"
 	exit 1
