988305c631aedf2467e3f37ee4f4f215c23c1c6f
Johnathan Van Why <jrvanwhy@google.com>
2020-07-08 15:50:45 -0700

    Add the `libtock_platform` crate.
    Currently, the crate is just a skeleton. I will add functionality in future PRs (I am merging an empty crate so that I can send PRs in parallel in the future).
    
    I will be replacing the system call layer in libtock_core::syscalls with a new layer in libtock_platform. libtock_core depends on libtock_platform and will re-export its functionality. This work is tracked at https://github.com/tock/libtock-rs/issues/217

---
+++ core/platform/src/lib.rs
@@ -0,0 +1,9 @@
+#![no_std]
+
+// TODO: Implement this crate, which will be done piece-by-piece. Platform will
+// include:
+//   1. The Allowed and AllowedSlice abstractions for sharing memory with the
+//      kernel
+//   2. The PlatformApi trait and Platform implementation.
+//   3. A system call trait so that Platform works in both real Tock apps and
+//      unit test environments.
