5d985b362dd06b22549e87d4255589d0ab1929d8
Alistair Francis <alistair.francis@wdc.com>
2020-11-11 15:44:27 -0800

    tools/flash.sh: Automatically generate the permissions
    Update the `flash.sh` script to automatically generate the app
    permissions. This requires the elf2tab PR: https://github.com/tock/elf2tab/pull/28
    to accept the --permissions argument.
    
    This script will parse the output to determine what capsules are being
    used in the app. It then allows all of the commands in libtock-rs that
    can be used by those capsules.
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- tools/flash.sh
+++ tools/flash.sh
@@ -1,6 +1,45 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
+
+get_drivers() {
+    # List all of the drivers in the IR output
+    driver_output=$(cat ${rust_target_folder}/release/examples/${artifact}*.ll | grep "^%\"libtock::")
+    # Remove all of the parts we don't need to get a nice list
+    used_drivers=$(echo "${driver_output}" | cut -d ":" -f 3 | sort -u)
+
+    for driver in ${used_drivers}; do
+        # Get the full official kernel name
+        cat tock/capsules/src/driver.rs | grep -i ${driver} | tr -s ' ' | cut -d ' ' -f 2
+    done
+}
+
+get_driver_numbers_and_perms() {
+    # Lookup in the kernel what driver number is used (in hex).
+    driver_nums=$(for driver in ${1}; do
+        cat tock/capsules/src/driver.rs | grep -i ${driver} | tr -s ' ' | cut -d ' ' -f 4 | cut -d , -f 1 | cut -d x -f 2
+    done)
+
+    for driver in ${driver_nums}; do
+        # Convert the number to a decimal value
+        driver_base_ten=$(echo "obase=10; ibase=16; ${driver}" | bc)
+
+        # We now know the driver.
+        # We don't have an easy way to figure out what commands we are using.
+        # So instead we just allow all of them by default.
+
+        # Get a list of all commands in libtock-s
+        name=$(cat tock/capsules/src/driver.rs | grep ${driver} | tr -s ' ' | cut -d ' ' -f 2)
+        lower_name=$(echo "$name" | awk '{print tolower($0)}')
+
+        # Get all of the command numbers
+        command_nums=$(sed -n '/mod command_nr {/,/}/p' src/${lower_name}.rs | grep usize | tr -s ' ' | cut -d ' ' -f 7 | cut -d ';' -f 1)
+
+        for num in ${command_nums}; do
+            echo ${driver_base_ten},${num}
+        done
+    done
+}
 
 artifact="$(basename $1)"
 rust_target_folder="$(cd $(dirname $1)/../.. && pwd -P)"
@@ -70,7 +109,16 @@ cp "$1" "${elf_file_name}"
 
 STACK_SIZE=$(nm --print-size --size-sort --radix=d "${elf_file_name}" | grep STACK_MEMORY | cut -d " " -f 2)
 
-elf2tab -n "${artifact}" -o "${tab_file_name}" "${elf_file_name}" --stack ${STACK_SIZE} --app-heap $APP_HEAP_SIZE --kernel-heap $KERNEL_HEAP_SIZE --protected-region-size=64
+drivers=$(get_drivers)
+
+echo "This app uses the following drivers"
+echo ${drivers}
+
+perms=$(get_driver_numbers_and_perms "${drivers}")
+
+elf2tab -n "${artifact}" -o "${tab_file_name}" "${elf_file_name}" \
+        --stack ${STACK_SIZE} --app-heap $APP_HEAP_SIZE --kernel-heap $KERNEL_HEAP_SIZE \
+        --protected-region-size=64 --permissions ${perms} --verbose
 
 if [ $tockload == "n" ]; then
 	echo "Skipping flashing for platform \"${PLATFORM}\""
