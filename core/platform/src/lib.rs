c9e5fc167a6eef96d820a674781abcded144c95f
Johnathan Van Why <jrvanwhy@google.com>
2020-11-28 19:45:06 -0800

    Remove `libtock_platform::allows`.
    Tock 2.0 system calls do not need the types in `allows`. Now that I'm reworking `libtock_platform` to use Tock 2.0 system calls, we can get rid of the module.

--- core/platform/src/lib.rs
+++ core/platform/src/lib.rs
@@ -1,17 +1,7 @@
 #![no_std]
 
-// TODO: Implement this crate, which will be done piece-by-piece. Platform will
-// include:
-//   1. The Allowed and AllowedSlice abstractions for sharing memory with the
-//      kernel
-//   2. The PlatformApi trait and Platform implementation.
-//   3. A system call trait so that Platform works in both real Tock apps and
-//      unit test environments. [DONE]
-
-mod allows;
 mod error_code;
 mod syscalls;
 
-pub use allows::{AllowReadable, Allowed};
 pub use error_code::ErrorCode;
 pub use syscalls::{MemopNoArg, MemopWithArg, Syscalls};
