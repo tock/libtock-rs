f7617f69c59706adc9b7391aba29b3554e708dd5
Johnathan Van Why <jrvanwhy@google.com>
2021-04-09 23:21:24 -0700

    Add the `libtock_unittest` crate.
    `libtock_unittest` will be needed in order to unit test `libtock-rs` code that makes system calls. This change contains a small fraction of the library, and intended to be followed up with several more changes that add system call implementations.
    
    I made careful use of `// TODO` comments and `#[allow(unused)]` declarations so that I can send all the system call implementations for review in parallel without causing massive merge conflicts.

---
+++ unittest/src/syscall_log.rs
@@ -0,0 +1,10 @@
+/// SyscallLogEntry represents a system call made during test execution.
+#[derive(Debug, PartialEq)]
+pub enum SyscallLogEntry {
+    // TODO: Add Yield.
+// TODO: Add Subscribe.
+// TODO: Add Command.
+// TODO: Add Allow.
+// TODO: Add Memop.
+// TODO: Add Exit.
+}
