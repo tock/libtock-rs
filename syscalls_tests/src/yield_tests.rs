7abef67bdc8eb69c270bb8f68ca8df6804e5bfca
Johnathan Van Why <jrvanwhy@google.com>
2021-05-05 16:34:25 -0700

    Add tests for the Yield system call implementation.

---
+++ syscalls_tests/src/yield_tests.rs
@@ -0,0 +1,38 @@
+//! Tests for implementations of Yield system calls in
+//! `libtock_platform::Syscalls`.
+
+use libtock_platform::{Syscalls, YieldNoWaitReturn};
+use libtock_unittest::{fake, ExpectedSyscall, SyscallLogEntry};
+
+// Tests yield_no_wait with an upcall executed.
+#[test]
+fn no_wait_upcall() {
+    use YieldNoWaitReturn::Upcall;
+    let kernel = fake::Kernel::new();
+    kernel.add_expected_syscall(ExpectedSyscall::YieldNoWait {
+        override_return: Some(Upcall),
+    });
+    assert_eq!(fake::Kernel::yield_no_wait(), Upcall);
+    assert_eq!(kernel.take_syscall_log(), [SyscallLogEntry::YieldNoWait]);
+}
+
+// Tests yield_no_wait with no upcall executed.
+#[test]
+fn no_wait_no_upcall() {
+    use YieldNoWaitReturn::NoUpcall;
+    let kernel = fake::Kernel::new();
+    kernel.add_expected_syscall(ExpectedSyscall::YieldNoWait {
+        override_return: Some(NoUpcall),
+    });
+    assert_eq!(fake::Kernel::yield_no_wait(), NoUpcall);
+    assert_eq!(kernel.take_syscall_log(), [SyscallLogEntry::YieldNoWait]);
+}
+
+// Tests yield_wait.
+#[test]
+fn wait() {
+    let kernel = fake::Kernel::new();
+    kernel.add_expected_syscall(ExpectedSyscall::YieldWait { skip_upcall: true });
+    fake::Kernel::yield_wait();
+    assert_eq!(kernel.take_syscall_log(), [SyscallLogEntry::YieldWait]);
+}
