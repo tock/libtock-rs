ae26831399aa18b88dc06075bb5bd4aac4105386
Johnathan Van Why <jrvanwhy@google.com>
2021-05-13 16:36:40 -0700

    Implement Yield in `libtock_unittest::fake::Kernel`.
    This Yield implementation will be used by the unit tests of the `libtock_platform::Syscalls` yield calls. This implementation is currently partial, as the `fake::Driver` trait and upcall queue have not been implemented yet.
    
    I also changed `libtock_platform::YieldNoWaitReturn` to use the new upcall terminology rather than callback.

--- unittest/src/expected_syscall.rs
+++ unittest/src/expected_syscall.rs
@@ -2,21 +2,33 @@
 /// a particular system call. An example use case is error injection: unit tests
 /// can add a `ExpectedSyscall` to the fake kernel's queue to insert errors in
 /// order to test error handling code.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum ExpectedSyscall {
-    // TODO: Add Yield.
-// TODO: Add Subscribe.
-// TODO: Add Command.
-// TODO: Add Allow.
-// TODO: Add Memop.
-// TODO: Add Exit.
+    // -------------------------------------------------------------------------
+    // Yield
+    // -------------------------------------------------------------------------
+    YieldNoWait {
+        /// If not `None`, `yield-no-wait` will set the return value to the
+        /// specified value. If `None`, `yield-no-wait` will set the return
+        /// value based on whether or not an upcall was run.
+        override_return: Option<libtock_platform::YieldNoWaitReturn>,
+    },
+
+    YieldWait {
+        /// If true, yield_wait will skip executing a upcall.
+        skip_upcall: bool,
+    },
+    // TODO: Add Subscribe.
+    // TODO: Add Command.
+    // TODO: Add Allow.
+    // TODO: Add Memop.
+    // TODO: Add Exit.
 }
 
 impl ExpectedSyscall {
     // Panics with a message describing that the named system call was called
     // instead of the expected system call. Used by fake::Kernel to report
     // incorrect system calls.
-    #[allow(unused)] // TODO: Remove when a system call is implemented.
     pub(crate) fn panic_wrong_call(&self, called: &str) -> ! {
         // TODO: Implement Display for ExpectedSyscall and replace {:?} with {}
         panic!(
