965abaacac115c403ab668ab9f9f1e83dc6078d2
Alyssa Haroldsen <kupiakos@google.com>
2021-12-07 14:53:52 -0800

    Use the ErrorCode enum directly

--- panic_handlers/small_panic/src/lib.rs
+++ panic_handlers/small_panic/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 
 use libtock_low_level_debug::{AlertCode, LowLevelDebug};
-use libtock_platform::Syscalls;
+use libtock_platform::{ErrorCode, Syscalls};
 use libtock_runtime::TockSyscalls;
 
 #[panic_handler]
@@ -10,5 +10,5 @@ fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
     LowLevelDebug::<TockSyscalls>::print_alert_code(AlertCode::Panic);
 
     // Exit with a non-zero exit code to indicate failure.
-    TockSyscalls::exit_terminate(1);
+    TockSyscalls::exit_terminate(ErrorCode::Fail as u32);
 }
