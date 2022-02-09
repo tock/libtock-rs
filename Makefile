a35f971586f7d38d1f776f345378f6f24ae8e516
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2022-02-09 19:10:19 +0200

    add armv6 and raspberry pi pico

--- Makefile
+++ Makefile
@@ -194,6 +194,15 @@ flash-nrf52840:
 	LIBTOCK_PLATFORM=nrf52840 cargo run --example $(EXAMPLE) $(features) \
 		--target=thumbv7em-none-eabi $(release) -- --deploy=tockloader
 
+.PHONY: raspberrypi_pico
+raspberrypi_pico:
+	LIBTOCK_PLATFORM=raspberrypi_pico cargo run --example $(EXAMPLE) $(features) \
+		--target=thumbv6m-none-eabi $(release)
+	mkdir -p target/tbf/raspberrypi_pico
+	cp target/thumbv6m-none-eabi/release/examples/$(EXAMPLE).tab \
+		target/thumbv6m-none-eabi/release/examples/$(EXAMPLE).tbf \
+		target/tbf/raspberrypi_pico
+
 .PHONY: stm32f3discovery
 stm32f3discovery:
 	LIBTOCK_PLATFORM=stm32f3discovery cargo run --example $(EXAMPLE) \
--- runtime/src/syscalls_impl_arm.rs
+++ runtime/src/syscalls_impl_arm.rs
@@ -27,6 +27,12 @@ unsafe impl RawSyscalls for crate::TockSyscalls {
         // Safety: This matches the invariants required by the documentation on
         // RawSyscalls::yield2
         unsafe {
+            #[cfg(all(
+                target_arch = "arm",
+                target_feature = "v7",
+                target_feature = "thumb-mode",
+                target_os = "none"
+                ))]
             asm!("svc 0",
                  inlateout("r0") r0 => _, // a1
                  inlateout("r1") r1 => _, // a2
@@ -36,10 +42,33 @@ unsafe impl RawSyscalls for crate::TockSyscalls {
                  // r9 is platform-specific. We don't use it in libtock_runtime,
                  // so it is either unused or used as a callee-saved register.
                  // r10 and r11 are callee-saved.
+                 
                  lateout("r12") _, // ip
                  // r13 is the stack pointer and must be restored by the callee.
                  lateout("r14") _, // lr
                  // r15 is the program counter.
+            );
+
+            #[cfg(all(
+                target_arch = "arm",
+                target_feature = "v6",
+                target_feature = "thumb-mode",
+                target_os = "none"
+                ))]
+            asm!("svc 0",
+                 inlateout("r0") r0 => _, // a1
+                 inlateout("r1") r1 => _, // a2
+                 lateout("r2") _,         // a3
+                 lateout("r3") _,         // a4
+                 // r4-r8 are callee-saved.
+                 // r9 is platform-specific. We don't use it in libtock_runtime,
+                 // so it is either unused or used as a callee-saved register.
+                 // r10 and r11 are callee-saved.
+                 
+                 // r13 is the stack pointer and must be restored by the callee.
+                 // r15 is the program counter.
+
+                 clobber_abi("C"), // ip (r12), lr (r14)
             );
         }
     }
