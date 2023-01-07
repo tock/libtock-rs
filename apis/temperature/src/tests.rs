dd9cc0c787b7827b417ade631255e549636bf7ce
Alexandru Carp <alexandrucarp02@gmail.com>
2023-01-07 11:37:10 +0200

    added lib tests

---
+++ apis/temperature/src/tests.rs
@@ -0,0 +1,57 @@
+use core::cell::Cell;
+use libtock_platform::{share, ErrorCode, Syscalls, YieldNoWaitReturn};
+use libtock_unittest::fake;
+
+type Temperature = super::Temperature<fake::Syscalls>;
+
+#[test]
+fn no_driver() {
+    let _kernel = fake::Kernel::new();
+    assert_eq!(Temperature::exists(), Err(ErrorCode::NoDevice));
+}
+
+#[test]
+fn driver_check() {
+    let kernel = fake::Kernel::new();
+    let driver = fake::Temperature::new();
+    kernel.add_driver(&driver);
+
+    assert_eq!(Temperature::exists(), Ok(()));
+}
+
+#[test]
+fn read_temperature() {
+    let kernel = fake::Kernel::new();
+    let driver = fake::Temperature::new();
+    kernel.add_driver(&driver);
+
+    assert_eq!(Temperature::read_temperature(), Ok(()));
+    assert!(driver.is_busy());
+
+    assert_eq!(Temperature::read_temperature(), Err(ErrorCode::Busy));
+}
+
+#[test]
+fn register_unregister_listener() {
+    let kernel = fake::Kernel::new();
+    let driver = fake::Temperature::new();
+    kernel.add_driver(&driver);
+
+    let listener = Cell::<Option<(u32,)>>::new(None);
+    share::scope(|subscribe| {
+        assert_eq!(Temperature::read_temperature(), Ok(()));
+        driver.set_value(100);
+        assert_eq!(fake::Syscalls::yield_no_wait(), YieldNoWaitReturn::NoUpcall);
+
+        assert_eq!(Temperature::register_listener(&listener, subscribe), Ok(()));
+        assert_eq!(Temperature::read_temperature(), Ok(()));
+        driver.set_value(100);
+        assert_eq!(fake::Syscalls::yield_no_wait(), YieldNoWaitReturn::Upcall);
+        assert_eq!(listener.get(), Some((100,)));
+
+        Temperature::unregister_listener();
+        assert_eq!(Temperature::read_temperature(), Ok(()));
+        driver.set_value(100);
+        assert_eq!(fake::Syscalls::yield_no_wait(), YieldNoWaitReturn::NoUpcall);
+    });
+}
