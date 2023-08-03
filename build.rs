c3772fb354f6826019a693ba944bc8e3ef7487c4
Brad Campbell <bradjc5@gmail.com>
2023-08-03 16:56:18 -0400

    no longer copy linker files around
    Just point the linker to where they are already stored.

--- build.rs
+++ build.rs
@@ -1,5 +1,4 @@
 fn auto_layout() {
-    use std::fs::copy;
     use std::fs::File;
     use std::io::Write;
     use std::path::PathBuf;
@@ -7,7 +6,6 @@ fn auto_layout() {
     const PLATFORM_CFG_VAR: &str = "LIBTOCK_PLATFORM";
     const LINKER_FLASH_CFG_VAR: &str = "LINKER_FLASH";
     const LINKER_RAM_CFG_VAR: &str = "LINKER_RAM";
-    const LAYOUT_GENERIC_FILENAME: &str = "libtock_layout.ld";
 
     // Note: we need to print these rerun-if commands before using the variable
     // or file, so that if the build script fails cargo knows when to re-run it.
@@ -27,9 +25,9 @@ fn auto_layout() {
         "Build path contains a newline, which is unsupported"
     );
 
-    // Where we are going to put the custom linker script for this particular
-    // build.
-    let out_platform_path: PathBuf = [out_dir, "layout.ld"].iter().collect();
+    // Point the linker to the generic libtock-rs linker file.
+    let generic_ld_dir: PathBuf = ["runtime"].iter().collect();
+    println!("cargo:rustc-link-search={}", generic_ld_dir.display());
 
     // Choose the linker file we are going to use for this build. That can be
     // specified by choosing a platform, where the linker file will be selected
@@ -45,14 +43,18 @@ fn auto_layout() {
     let linker_ram = std::env::var(LINKER_RAM_CFG_VAR);
 
     if let Ok(platform) = platform {
-        // Copy the platform-specific layout file into OUT_DIR.
-        let platform_filename = format!("{}.ld", platform);
-        let platform_path: PathBuf = ["runtime", "layouts", &platform_filename].iter().collect();
-        println!("cargo:rerun-if-changed={}", platform_path.display());
-        assert!(platform_path.exists(), "Unknown platform {}", platform);
-        copy(&platform_path, out_platform_path)
-            .expect("Unable to copy platform layout into OUT_DIR");
-        println!("cargo:rustc-link-arg=-Tlayout.ld");
+        // Point the linker to the correct platform-specific linker file.
+        let platform_ld_name = format!("{}.ld", platform);
+        let platform_ld_dir: PathBuf = ["runtime", "layouts"].iter().collect();
+        let platform_ld_path: PathBuf = ["runtime", "layouts", &platform_ld_name].iter().collect();
+
+        assert!(platform_ld_path.exists(), "Unknown platform {}", platform);
+
+        println!("cargo:rerun-if-changed={}", platform_ld_path.display());
+        println!("cargo:rustc-link-arg=-T{}", platform_ld_name);
+
+        // Tell rustc where to search for the layout file.
+        println!("cargo:rustc-link-search={}", platform_ld_dir.display());
     } else if let (Ok(linker_flash), Ok(linker_ram)) = (linker_flash, linker_ram) {
         // Create a valid linker file with the specified flash and ram locations.
         //
@@ -79,20 +81,12 @@ fn auto_layout() {
 
         // Pass the name of this linker script to rustc.
         println!("cargo:rustc-link-arg=-T{}", linker_script_name);
-        // println!("cargo:rustc-link-arg-bins=-T{}", linker_script_name);
+
+        // Tell rustc where to search for the layout file.
+        println!("cargo:rustc-link-search={}", out_dir);
     } else {
         panic!("Need to set LIBTOCK_PLATFORM or (LINKER_FLASH and LINKER_RAM)");
     }
-
-    // Copy the generic layout file into OUT_DIR.
-    let in_layout_generic: PathBuf = ["runtime", LAYOUT_GENERIC_FILENAME].iter().collect();
-    let out_layout_generic: PathBuf = [out_dir, LAYOUT_GENERIC_FILENAME].iter().collect();
-    println!("cargo:rerun-if-changed={}", LAYOUT_GENERIC_FILENAME);
-    copy(in_layout_generic, out_layout_generic)
-        .expect("Unable to copy layout_generic.ld into OUT_DIR");
-
-    // Tell rustc where to search for the layout file.
-    println!("cargo:rustc-link-search={}", out_dir);
 }
 
 fn main() {
