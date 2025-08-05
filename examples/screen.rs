d1bf0c66c5ddffb3af574f6eddb8018bb8804327
Ines-Maria <inesmaria.moraru@gmail.com>
2025-08-05 18:49:39 +0300

    Screen API implementation

---
+++ examples/screen.rs
@@ -0,0 +1,222 @@
+#![no_main]
+#![no_std]
+use core::fmt::Write;
+use libtock::alarm::Alarm;
+use libtock::alarm::Milliseconds;
+use libtock::console::Console;
+use libtock::display::Screen;
+use libtock::runtime::{set_main, stack_size};
+
+set_main! {main}
+stack_size! {800}
+
+fn main() {
+    // Output a message to the console
+    let _ = writeln!(Console::writer(), "available resolutions\n");
+
+    // Query the number of supported Screen resolution modes
+    let resolutions = match Screen::get_resolution_modes_count() {
+        Ok(val) => val,
+        Err(e) => {
+            let _ = writeln!(Console::writer(), "{:?}\n", e);
+            0
+        }
+    };
+
+    // Exit if no resolutions found
+    if resolutions == 0 {
+        assert_eq!(0, 1);
+    }
+
+    let _ = writeln!(Console::writer(), "{resolutions}\n");
+
+    // Iterate over each resolution mode and print the width and height
+    for index in 0..resolutions {
+        let (width, height) = match Screen::get_resolution_width_height(index as usize) {
+            Ok((width, height)) => (width, height),
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+                (0, 0)
+            }
+        };
+
+        let width = (width, height).0;
+        let height = (width, height).1;
+
+        // Abort if invalid resolution
+        if width == 0 && height == 0 {
+            assert_eq!(0, 1);
+        }
+
+        let _ = writeln!(Console::writer(), " {width} x {height} \n");
+    }
+
+    // Print supported color depths
+    let _ = writeln!(Console::writer(), "available colors depths\n");
+
+    let pixel_format = match Screen::get_pixel_format() {
+        Ok(val) => val,
+        Err(e) => {
+            let _ = writeln!(Console::writer(), "{:?}\n", e);
+            0
+        }
+    };
+
+    if pixel_format == 0 {
+        assert_eq!(0, 1);
+    }
+
+    // List each supported color format
+    for index in 0..pixel_format {
+        let format = match Screen::pixel_format(index as usize) {
+            Ok(val) => val,
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+                0
+            }
+        };
+        let _ = writeln!(Console::writer(), "  {format} bbp\n");
+    }
+
+    // Initialize the Screen screen buffer
+    let mut buffer: Option<*mut u8> = None;
+    let status = Screen::screen_buffer_init(&mut buffer);
+    let _ = writeln!(Console::writer(), "screen init\n");
+
+    // Set Screen brightness to 100%
+    let _ = match Screen::set_brightness(100) {
+        Ok(()) => (),
+        Err(e) => {
+            let _ = writeln!(Console::writer(), "{:?}\n", e);
+        }
+    };
+
+    // Get current screen resolution
+    let (width, height) = match Screen::get_resolution() {
+        Ok((width, height)) => (width, height),
+        Err(e) => {
+            let _ = writeln!(Console::writer(), "{:?}\n", e);
+            (0, 0)
+        }
+    };
+
+    // Unwrap width and height
+    let width = (width, height).0;
+    let height = (width, height).1;
+
+    if width == 0 && height == 0 {
+        assert_eq!(0, 1);
+    };
+
+    // Set full-screen write frame and clear screen
+    let _ = match Screen::set_write_frame(0, 0, width, height) {
+        Ok(()) => (),
+        Err(e) => {
+            let _ = writeln!(Console::writer(), "{:?}\n", e);
+        }
+    };
+    let _ = match Screen::fill(status, 0x0) {
+        Ok(()) => (),
+        Err(e) => {
+            let _ = writeln!(Console::writer(), "{:?}\n", e);
+        }
+    };
+
+    // Animation loop: cycle through rotations and color block updates
+    let mut invert = false;
+    for i in 0.. {
+        // Every 4 iterations, toggle Screen inversion
+        if i % 4 == 3 {
+            invert = !invert;
+            if invert {
+                let _ = Screen::set_invert_on();
+            } else {
+                let _ = Screen::set_invert_off();
+            }
+        }
+
+        // Set Screen rotation (0 to 3)
+        let _ = match Screen::set_rotation(i % 4) {
+            Ok(()) => (),
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+            }
+        };
+
+        // Draw a red square at (10, 20)
+        let _ = match Screen::set_write_frame(10, 20, 30, 30) {
+            Ok(()) => (),
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+            }
+        };
+        let _ = match Screen::fill(status, 0xF800) {
+            Ok(()) => (),
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+            }
+        };
+
+        // Draw a black square at (88, 20)
+        let _ = match Screen::set_write_frame(88, 20, 30, 30) {
+            Ok(()) => (),
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+            }
+        };
+        let _ = match Screen::fill(status, 0x0) {
+            Ok(()) => (),
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+            }
+        };
+
+        // Wait 1 second
+        Alarm::sleep_for(Milliseconds(1000)).unwrap();
+
+        // Clear the red square
+        let _ = match Screen::set_write_frame(10, 20, 30, 30) {
+            Ok(()) => (),
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+            }
+        };
+        let _ = match Screen::fill(status, 0x0) {
+            Ok(()) => (),
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+            }
+        };
+
+        // Draw a green square at (88, 20)
+        let _ = match Screen::set_write_frame(88, 20, 30, 30) {
+            Ok(()) => (),
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+            }
+        };
+        let _ = match Screen::fill(status, 0x07F0) {
+            Ok(()) => (),
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+            }
+        };
+
+        // Wait 1 second
+        Alarm::sleep_for(Milliseconds(1000)).unwrap();
+
+        // Clear screen
+        let _ = match Screen::set_write_frame(0, 0, width, height) {
+            Ok(()) => (),
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+            }
+        };
+        let _ = match Screen::fill(status, 0x0) {
+            Ok(()) => (),
+            Err(e) => {
+                let _ = writeln!(Console::writer(), "{:?}\n", e);
+            }
+        };
+    }
+}
